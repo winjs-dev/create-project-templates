24cf3864ee9a63bc85299ea06d5afcf7f0e9c045
cklwblove <lwbhtml@163.com>
2022-08-01 09:09:05 +0800

    chore(version): vue3 版本升级

--- utils/renderTemplate.js
+++ utils/renderTemplate.js
@@ -17,6 +17,11 @@ import sortDependencies from './sortDependencies.js';
 function renderTemplate(src, dest, options = {}) {
   const stats = fs.statSync(src);
   if (stats.isDirectory()) {
+    // skip node_module
+    if (path.basename(src) === 'node_modules') {
+      return;
+    }
+
     // if it's a directory, render its subdirectories and files recusively
     fs.mkdirSync(dest, { recursive: true });
     for (const file of fs.readdirSync(src)) {
@@ -29,8 +34,8 @@ function renderTemplate(src, dest, options = {}) {
 
   if (filename === 'package.json' && fs.existsSync(dest)) {
     // merge instead of overwriting
-    const existing = JSON.parse(fs.readFileSync(dest));
-    const newPackage = JSON.parse(fs.readFileSync(src));
+    const existing = JSON.parse(fs.readFileSync(dest, 'utf8'));
+    const newPackage = JSON.parse(fs.readFileSync(src, 'utf8'));
     const pkg = sortDependencies(deepMerge(existing, newPackage));
     fs.writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n');
     return;
