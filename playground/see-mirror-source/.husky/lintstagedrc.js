56af450e2ed2f4d9501d209ec79b294a95c0bb10
cklwblove <lwbhtml@163.com>
2022-08-23 09:21:07 +0800

    chore(playground): 更新 playground

--- playground/see-mirror-source/.husky/lintstagedrc.js
+++ playground/see-mirror-source/.husky/lintstagedrc.js
@@ -4,6 +4,6 @@ module.exports = {
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write --parser json'],
   'package.json': ['prettier --write'],
   '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write', 'git add .'],
-  '*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write', 'git add .'],
+  '!(index)*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write', 'git add .'],
   '*.md': ['prettier --write']
 };
