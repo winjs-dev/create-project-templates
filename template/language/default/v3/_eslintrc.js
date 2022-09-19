f2a9e6fe528ac62f5bbc5e2167ae99ddbcbddb51
cklwblove <lwbhtml@163.com>
2022-09-19 16:35:23 +0800

    refactor: 💡 移除不必要的依赖

--- template/language/default/v3/_eslintrc.js
+++ template/language/default/v3/_eslintrc.js
@@ -1,10 +1,5 @@
 // http://eslint.org/docs/user-guide/configuring
 // http://eslint.cn/docs/user-guide/configuring 中文，注意版本是否和官网一致
 module.exports = {
-  extends: [
-    '@winner-fed/win',
-    '@winner-fed/win/vue3',
-    'plugin:prettier/recommended',
-    'vue-global-api'
-  ]
+  extends: ['@winner-fed/win', '@winner-fed/win/vue3', 'plugin:prettier/recommended']
 };
