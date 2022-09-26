5c7d70f97f2d1e04c11c8bdbe8f7094347f10d20
cklwblove <lwbhtml@163.com>
2022-09-26 14:53:38 +0800

    version 2.3.8 snapshot

---
+++ typescript-see/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="typescriptSeefd8ng4a4bi" class="typescript-see-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+
+  @Component({
+    name: 'App'
+  })
+  export default class App extends Vue {}
+</script>
