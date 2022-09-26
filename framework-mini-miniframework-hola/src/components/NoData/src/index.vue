48a1fb827ea2cd7b35f6ce95f33285d3a13a3633
cklwblove <lwbhtml@163.com>
2022-09-26 11:06:26 +0800

    version 1.0.0 snapshot

---
+++ framework-mini-miniframework-hola/src/components/NoData/src/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="nodata-card" :class="{ small: small, smaller: smaller }">
+    <view class="content no-comment">
+      <view class="member-card">
+        <image v-if="night" class="img" src="../../../assets/images/bg_no_data_night.png"></image>
+        <image v-else class="img" src="../../../assets/images/bg_no_data.png"></image>
+      </view>
+      <view class="no-data">{{ datadec }}</view>
+    </view>
+  </view>
+</template>
+<script type="text/babel">
+  export default {
+    props: ['datadec', 'small', 'smaller', 'night'],
+    name: 'NoData'
+  };
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  @import 'style.less';
+</style>
