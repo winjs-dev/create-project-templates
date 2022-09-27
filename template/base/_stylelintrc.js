9ccc5609dbc44702fd2007e7f9dd15da37674343
cklwblove <lwbhtml@163.com>
2022-09-27 16:13:07 +0800

    fix: 🐛 修复 stylelint 升级14出现的问题

--- template/base/_stylelintrc.js
+++ template/base/_stylelintrc.js
@@ -1,5 +1,7 @@
 // https://stylelint.io/
 // https://stylelint.docschina.org/user-guide/rules/ 中文，注意版本是否和官网一致
 module.exports = {
-  extends: '@winner-fed/stylelint-config-win'
+  extends: '@winner-fed/stylelint-config-win',
+  // 指定 less 语法
+  customSyntax: 'postcss-less'
 };
