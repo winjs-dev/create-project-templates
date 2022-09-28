40ac72d201aef7483aa60181cee9837929f7923f
cklwblove <lwbhtml@163.com>
2022-09-28 13:25:54 +0800

    version 2.3.13 snapshot

---
+++ mirror-source/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './router/router.interceptor';
+import './components/global';
+import './filters';
+import './services';
+import './plugins/composition';
+import './assets/style/app.less';
+
+/* eslint-disable */
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#mirrorSource29b3464wlzt',
+  router,
+  // use Runtime-only
+  // https://vuejs.org/v2/guide/installation.html
+  render: (h) => h(App)
+});
