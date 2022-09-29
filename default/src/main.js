37b0c078ff932dc5fd1506366a8c86dab9949b00
cklwblove <lwbhtml@163.com>
2022-09-29 19:19:43 +0800

    version 2.3.16 snapshot

---
+++ default/src/main.js
@@ -0,0 +1,24 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './router/router.interceptor';
+import './components/global';
+import './icons';
+import './filters';
+import './services';
+import './plugins/composition';
+import './assets/style/app.less';
+
+/* eslint-disable */
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#default4fpltfp0x54',
+  router,
+  // use Runtime-only
+  // https://vuejs.org/v2/guide/installation.html
+  render: (h) => h(App)
+});
---
+++ see/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="see7g7q3fa92ww" class="see-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
