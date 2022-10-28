ff08c2888814da6ca44300f66c3377137cb21ae4
cklwblove <lwbhtml@163.com>
2022-10-28 16:56:23 +0800

    version 2.3.20 snapshot

---
+++ mirror-source/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mirrorSource2n5d31mn2mg" class="mirror-source-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
---
+++ typescript-see-mirror-source/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="typescriptSeeMirrorSource5gagy6b24s" class="typescript-see-mirror-source-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+
+  @Component({
+    name: 'App'
+  })
+  export default class App extends Vue {}
+</script>
