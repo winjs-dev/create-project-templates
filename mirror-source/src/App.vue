94648e5feec18dc45420e51f9e08b263976d90d5
cklwblove <lwbhtml@163.com>
2022-11-29 15:17:40 +0800

    version 2.3.23 snapshot

---
+++ mirror-source/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mirrorSourcepplc0ykr7c" class="mirror-source-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
---
+++ see/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="see10hlip48qrx" class="see-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
