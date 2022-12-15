0364ff6cd68a7a4855c516c447331fb2a18561ae
cklwblove <lwbhtml@163.com>
2022-12-15 14:00:08 +0800

    feat: 🎸 添加sort-package-json，自动排序package.json

--- template/version-control/default/.husky/nano-staged.js
+++ template/version-control/default/.husky/nano-staged.js
@@ -2,7 +2,7 @@ module.exports = {
   // 对指定格式文件 在提交的时候执行相应的修复命令
   'src/**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add .'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write --parser json'],
-  'package.json': ['prettier --write'],
+  'package.json': ['prettier --write', 'sort-package-json'],
   'src/**/*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write', 'git add .'],
   'src/**/*.{scss,less,styl,css}': ['stylelint --fix', 'prettier --write', 'git add .'],
   '*.md': ['prettier --write']
