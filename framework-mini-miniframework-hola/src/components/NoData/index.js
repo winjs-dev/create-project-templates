94efd3712cad9d51bd3e60fa667b4125fc151347
cklwblove <lwbhtml@163.com>
2023-01-06 10:49:23 +0800

    version 2.3.28 snapshot

---
+++ framework-mini-miniframework-hola/src/components/NoData/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hspcadmin on 2017/3/31.
+ */
+export { default } from './src/index.vue';
+
+/*
+  使用方法
+ * <nodata :datadec="dec" :small='date'></nodata>
+ * 在父组件中添加描述信息：dec：'',
+ * small布尔值，如果你想要小尺寸的暂无数据，就设置这个词为true
+ */
---
+++ typescript-mirror-source/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="typescriptMirrorSource2jtk9zn970i" class="typescript-mirror-source-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+
+  @Component({
+    name: 'App'
+  })
+  export default class App extends Vue {}
+</script>
