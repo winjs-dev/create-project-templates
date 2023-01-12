bba874c2cac0fb4ea062ba2de763030eaeab68f7
cklwblove <lwbhtml@163.com>
2023-01-12 16:20:56 +0800

    version 2.3.29 snapshot

---
+++ default/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="defaultp8fpdokncq" class="default-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
