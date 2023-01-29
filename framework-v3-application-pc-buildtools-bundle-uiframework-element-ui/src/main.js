fc21adde59907198dc707f594571cd85739bdcec
cklwblove <lwbhtml@163.com>
2023-01-29 18:53:13 +0800

    version 2.3.30 snapshot

---
+++ framework-v3-application-pc-buildtools-bundle-uiframework-element-ui/src/main.js
@@ -0,0 +1,31 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router, { setupRouter } from './router';
+import './router/router.interceptor';
+import { setGlobalProperties } from '@/services';
+import './icons';
+import setupGlobalComponent from '@/components/global';
+import { setApp } from './useApp';
+import setupVendor from './vendor/element';
+import './assets/style/app.less';
+
+async function bootstrap() {
+  const app = createApp(App);
+
+  setupGlobalComponent(app);
+  setGlobalProperties(app);
+  setupVendor(app);
+  setupRouter(app);
+
+  // Mount when the route is ready
+  // https://next.router.vuejs.org/api/#isready
+  await router.isReady();
+  app.mount('#frameworkV3ApplicationPcBuildtoolsBundleUiframeworkElementUi1u94jahe6mg', true);
+
+  setApp(app);
+}
+
+bootstrap();
+
---
+++ see/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="see1i5bu2o11t6" class="see-container">
+    <div class="pages">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view />
+      </keep-alive>
+      <router-view v-if="!$route.meta.keepAlive" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  };
+</script>
