63655c6e5497bb645f967b96c89b94096b8acaac
liwb <lwbhtml@163.com>
2025-05-12 14:03:00 +0800

    refactor: 💡 index.html 的 meta 修改
    apple-mobile-web-app-capable 替换为 mobile-web-app-capable

--- utils/generateIndexHTML.ts
+++ utils/generateIndexHTML.ts
@@ -11,7 +11,7 @@ const indexHTML = `<!DOCTYPE html>
       name="viewport"
       content="width=device-width, viewport-fit=cover, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
     />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <meta content="telephone=no" name="format-detection" />
     <meta content="email=no" name="format-detection" />
